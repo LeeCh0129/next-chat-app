eff0ba0f4ce1ba5c0c3963fef339771a49df6ba0
LeeCh0129 <cksgh5477@gmail.com>
2024-07-16 13:28:57 +0900

    DesktopItem Component

--- src/components/sidebar/DesktopItem.tsx
+++ src/components/sidebar/DesktopItem.tsx
@@ -1,7 +1,55 @@
+import clsx from "clsx";
+import Link from "next/link";
 import React from "react";
+import { IconType } from "react-icons/lib";
 
-const DesktopItem = () => {
-  return <div>DesktopItem</div>;
+interface DesktopItemProps {
+  label: string;
+  icon: IconType;
+  href: string;
+  onClick?: () => void;
+  active?: boolean;
+}
+
+const DesktopItem = ({
+  label,
+  href,
+  icon: Icon,
+  active,
+  onClick,
+}: DesktopItemProps) => {
+  const handleClick = () => {
+    if (onClick) {
+      return onClick();
+    }
+  };
+
+  return (
+    <li onClick={handleClick}>
+      <Link
+        href={href}
+        className={clsx(
+          `
+        group
+        flex
+        gap-x-3
+        rounded-md
+        p-3
+        text-sm
+        leading-6
+        font-semibold
+        text-gray-500
+        hover:text-black
+        bg-white
+        hover:bg-gray-100`,
+          active && "bg-gray-200 text-black"
+        )}
+      >
+        <Icon className="w-6 h-6 shrink-0" />
+        <span className="sr-only">{label}</span>
+      </Link>
+    </li>
+  );
 };
 
 export default DesktopItem;
