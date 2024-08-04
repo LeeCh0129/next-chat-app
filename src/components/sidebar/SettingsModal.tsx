e6fe5ef9bfb039caaac8d0e3fadefa99f0488f3a
LeeCh0129 <cksgh5477@gmail.com>
2024-08-04 17:08:56 +0900

    Setting Modal UI

--- src/components/sidebar/SettingsModal.tsx
+++ src/components/sidebar/SettingsModal.tsx
@@ -1,10 +1,17 @@
-import { User } from "@prisma/client";
+"use client";
+
 import axios from "axios";
-import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import { toast } from "react-hot-toast";
+import { User } from "@prisma/client";
+import { CldUploadButton } from "next-cloudinary";
+
+import Input from "../inputs/Input";
 import Modal from "../modals/Modal";
+import Button from "../Button";
+import Image from "next/image";
+import { toast } from "react-hot-toast";
 
 interface SettingsModalProps {
   isOpen?: boolean;
@@ -12,11 +19,11 @@ interface SettingsModalProps {
   currentUser: User;
 }
 
-const SettingsModal = ({
+const SettingsModal: React.FC<SettingsModalProps> = ({
   isOpen,
   onClose,
-  currentUser,
-}: SettingsModalProps) => {
+  currentUser = {},
+}) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -43,17 +50,83 @@ const SettingsModal = ({
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
+
     axios
       .post("/api/settings", data)
       .then(() => {
         router.refresh();
         onClose();
       })
-      .catch(() => toast.error("에러가 났습니다."))
+      .catch(() => toast.error("Something went wrong!"))
       .finally(() => setIsLoading(false));
   };
 
-  return <div>SettingsModal</div>;
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="space-y-12">
+          <div className="pb-12 border-b border-gray-900/10">
+            <h2 className="text-base font-semibold leading-7 text-gray-900 ">
+              프로필
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              프로필을 수정하세요.
+            </p>
+
+            <div className="flex flex-col mt-10 gap-y-8">
+              <Input
+                disabled={isLoading}
+                label="Name"
+                id="name"
+                errors={errors}
+                required
+                register={register}
+              />
+              <div>
+                <label
+                  htmlFor="photo"
+                  className="block text-sm font-medium leading-6 text-gray-900 "
+                >
+                  Photo
+                </label>
+                <div className="flex items-center mt-2 gap-x-3">
+                  <Image
+                    width="48"
+                    height="48"
+                    className="rounded-full"
+                    src={
+                      image || currentUser?.image || "/images/placeholder.jpg"
+                    }
+                    alt="Avatar"
+                  />
+                  <CldUploadButton
+                    options={{ maxFiles: 1 }}
+                    onUpload={handleUpload}
+                    uploadPreset={
+                      process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_PRESET
+                    }
+                  >
+                    <Button disabled={isLoading} secondary type="button">
+                      Change
+                    </Button>
+                  </CldUploadButton>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex items-center justify-end mt-6 gap-x-6">
+          <Button disabled={isLoading} secondary onClick={onClose}>
+            Cancel
+          </Button>
+          <Button disabled={isLoading} type="submit">
+            Save
+          </Button>
+        </div>
+      </form>
+    </Modal>
+  );
 };
 
 export default SettingsModal;
