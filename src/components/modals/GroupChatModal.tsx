8c898f4e73a783624308f7f01e7d05ed9b3784b5
LeeCh0129 <cksgh5477@gmail.com>
2024-08-26 19:15:08 +0900

    Select Component

--- src/components/modals/GroupChatModal.tsx
+++ src/components/modals/GroupChatModal.tsx
@@ -1,13 +1,16 @@
 "use client";
-import { User } from "@prisma/client";
+
 import axios from "axios";
-import { useRouter } from "next/navigation";
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
-import toast from "react-hot-toast";
-import Modal from "./Modal";
+import { User } from "@prisma/client";
+
 import Input from "../inputs/Input";
+import Select from "../inputs/Select";
+import Modal from "./Modal";
 import Button from "../Button";
+import { toast } from "react-hot-toast";
 
 interface GroupChatModalProps {
   isOpen?: boolean;
@@ -15,7 +18,11 @@ interface GroupChatModalProps {
   users: User[];
 }
 
-const GroupChatModal = ({ isOpen, onClose, users = [] }) => {
+const GroupChatModal: React.FC<GroupChatModalProps> = ({
+  isOpen,
+  onClose,
+  users = [],
+}) => {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -46,16 +53,16 @@ const GroupChatModal = ({ isOpen, onClose, users = [] }) => {
         router.refresh();
         onClose();
       })
-      .catch(() => toast.error("에러가 났습니다."))
+      .catch(() => toast.error("Something went wrong!"))
       .finally(() => setIsLoading(false));
   };
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="space-y12">
+        <div className="space-y-12">
           <div className="pb-12 border-b border-gray-900/10">
-            <h2 className="text-base font-semibold leading-7 text-gray-900">
+            <h2 className="text-base font-semibold leading-7 text-gray-900 ">
               그룹 채팅 만들기
             </h2>
             <p className="mt-1 text-sm leading-6 text-gray-600">
@@ -69,6 +76,20 @@ const GroupChatModal = ({ isOpen, onClose, users = [] }) => {
                 errors={errors}
                 required
                 register={register}
+              />
+              <Select
+                disabled={isLoading}
+                label="Members"
+                options={users.map((user) => ({
+                  value: user.id,
+                  label: user.name,
+                }))}
+                onChange={(value) =>
+                  setValue("members", value, {
+                    shouldValidate: true,
+                  })
+                }
+                value={members}
               />
             </div>
           </div>
